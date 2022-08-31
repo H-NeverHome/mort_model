05aec280f61026925b5e344290ea8d7ff4672cf4
Hauke <42414352+H-NeverHome@users.noreply.github.com>
2022-08-31 23:55:50 +0200

    update

--- mort_DE.py
+++ mort_DE.py
@@ -1,16 +1,79 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Aug 27 11:31:37 2022
 
 @author: de_hauk
 """
-from sktime.datasets import load_airline
+
+
+#%%
+########################################
+#################### IMPORTS PYTHON
 from sktime.forecasting.arima import AutoARIMA,ARIMA
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import os 
+import rpy2
 
-data_path = r'C:\Users\de_hauk\Documents\GitHub\mort_model/Mx_1x1.txt'
+wdir = r'C:\Users\de_hauk\Documents\GitHub\mort_model'
+os.chdir(wdir)
+from mort_model_class import LC_model
+#%%
+########################################
+#################### IMPORTS R & RPY2
+
+r_home = r'C:\Program Files\R\R-4.0.2'
+os.environ['R_HOME'] = r_home
+
+rpy2_info = (rpy2.__path__,rpy2.__version__,'R_HOME',r_home)
+print(rpy2_info)
+
+def r_to_pd(smth_r):
+    from rpy2.robjects import pandas2ri
+    from rpy2.robjects.conversion import localconverter
+    import rpy2.robjects as ro
+    
+    with localconverter(ro.default_converter + pandas2ri.converter):
+        smth_pd   = ro.conversion.rpy2py(smth_r)
+    return smth_pd
+
+def pd_to_r(smth_pd):
+    from rpy2.robjects import pandas2ri
+    from rpy2.robjects.conversion import localconverter
+    import rpy2.robjects as ro
+    
+    with localconverter(ro.default_converter + pandas2ri.converter):
+        smth_r   = ro.conversion.py2rpy(smth_pd)
+    return smth_r
+
+from rpy2.robjects import globalenv
+from rpy2.robjects.packages import importr
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.conversion import localconverter
+import rpy2.robjects as ro
+
+base = importr('base')
+utils = importr('utils')
+brms = importr('brms')
+sjPlot = importr('sjPlot')
+ro.r('if (require(StMoMo) == FALSE) {install.packages("StMoMo")}')
+ro.r('if (require(demography) == FALSE) {install.packages("demography")}')
+ro.r('if (require(HMDHFDplus) == FALSE) {install.packages("HMDHFDplus")}')
+ro.r('if (require(MTS) == FALSE) {install.packages("MTS")}')
+ro.r('if (require(stats) == FALSE) {install.packages("stats")}')
+
+importr('MTS')
+importr('demography')
+importr('StMoMo')
+importr('HMDHFDplus')
+importr('stats')
+
+
+#%%
+########################################
+#################### IMPORT & PREPROCESS DATA FROM HMD DEATHRATES W_GER
+
+data_path = r'Mx_1x1.txt'
 raw_dat_wDE = pd.read_csv(filepath_or_buffer = data_path,
                             sep= '\t',
                             header = 1,
@@ -66,113 +129,91 @@ fin_dat.sort_index(axis=0,
 fin_dat[fin_dat>1.] = np.nan
 fin_dat[fin_dat==0] = np.nan
 
+      
 
-# import seaborn as sns
-# sns.set_context('paper')
-# ax = sns.heatmap(fin_dat, 
-#                  linewidths=.5,
-#                  annot=False,
-#                  xticklabels = [i for i in fin_dat],
-#                  yticklabels = [i for i in fin_dat.index],
-#                  square = True)
+
+
 #%%
+##############################################################################
+############################ GET STANDARD DATA from STMOMO and export to py
+#Age-specific deaths and exposures for England and Wales from the Human Mortality Database. 
+ro.r('EWMaleIniData <- central2initial(EWMaleData)')
 
+ro.r('exp1 <- EWMaleData$Dxt') #matrix of deaths
+ro.r('exp1_row <- rownames(EWMaleData$Dxt)')
+ro.r('exp1_clm <- colnames(EWMaleData$Dxt)')
 
-class LC_model:
-    
-    def __init__(self):
-        print('model init')
-        import pandas as pd
-        import numpy as np
-        from scipy.linalg import svd
+ro.r('exp2 <- EWMaleData$Ext') #matrix of pop
+ro.r('exp2_row <- rownames(EWMaleData$Ext)')
+ro.r('exp2_clm <- colnames(EWMaleData$Ext)')
 
-    
-    def load_data(self,data,data_type):
-        import pandas as pd
-        import numpy as np
-        from scipy.linalg import svd
-        error_msg_datatype = 'input/data-type not supported.'
-        error_mdg_data = '''Data needs to be dataframe of mortality rates.
-                            Years as COLUMNS and Age as ROWS'''
-        if data_type == 'rate':
-            self.mort_data_raw = data.dropna(axis = 0,
-                                             how = 'any').apply(func = np.log,
-                                                                axis = 1,
-                                                                raw = True)
-                                                  
-            self.a_x = self.mort_data_raw.mean(axis = 'columns')
-            
-            a_xt_raw = pd.DataFrame(index = [i for i in self.mort_data_raw.index])
-            for year in self.mort_data_raw:
-                curr_clm = self.mort_data_raw[year].values - self.a_x.values
-                a_xt_raw[year] = curr_clm.tolist()
-            
-            
-            self.a_xt = a_xt_raw.copy().T
-            
-            res_SVD = svd(self.a_xt.values,
-                          full_matrices=True)     
-            
-            ### Compute SVD of A_xt
-            # USV* = SVD(A_xt)  
-            # U == left-singular vectors
-            # S == singular values 
-            # V* == right-singular vectors                                               
-            self.res_SVD_fin = {'U' : res_SVD[0],
-                                'S' : res_SVD[1],
-                                'V*' : res_SVD[2]}
-            
-            #extract param k_t
-            self.k_t = pd.DataFrame(self.res_SVD_fin['U'])[0].values
-            
-            #extract param b_x
-            self.b_x = pd.DataFrame(self.res_SVD_fin['V*']).T[0].values        
-        else:
-            print()
-        
-    def fit_predict(self,n_futures, pred_int):
-        from sktime.forecasting.arima import ARIMA
-        self.future_N = n_futures
-        
-        arima_model = ARIMA(order=(1, 0, 1), 
-                            seasonal_order=(0, 0, 0, 0)).fit(self.k_t)
-        
-        self.arima_model = arima_model
-        self.future_lags = [i for i in range(self.future_N)]
-        self.y_pred = arima_model.predict(fh=self.future_lags).flatten().tolist()
-        self.y_pred_int = arima_model.predict_interval(fh = self.future_lags,
-                                                          coverage = pred_int)
+n_D = pd.DataFrame(data = r_to_pd(globalenv['exp1']),
+                   columns = list(globalenv['exp1_clm']),
+                   index = list(globalenv['exp1_row']))
+n_POP = pd.DataFrame(data = r_to_pd(globalenv['exp2']),
+                     columns = list(globalenv['exp2_clm']),
+                     index = list(globalenv['exp2_row']))
 
-        
-        self.mort_data = self.mort_data_raw.copy()
-        last_dat = int(self.mort_data.columns.max())
-        
-        for fut_year in range(self.future_N):
-            term1 = self.a_x+self.res_SVD_fin['S'][0]
-            term2 = self.b_x*self.y_pred[fut_year]
-            term3 = term1*term2
-            self.mort_data[str(last_dat+(fut_year+1))] = term3
-       
-        self.all_Y = self.mort_data.columns.tolist()
-        final_res_kt = pd.DataFrame(index = self.all_Y)
-        final_res_kt['kt_log'] = self.k_t.flatten().tolist() + self.y_pred
-        print(final_res_kt)
-        for i,j in zip(self.y_pred_int,[f'lower_{pred_int}',f'higher_{pred_int}']):
-            dat = self.y_pred_int[i].tolist() + self.y_pred
-            final_res_kt[j] = [0 for i in range(len(self.k_t.flatten().tolist()) )]+self.y_pred_int[i].tolist()
-       
-
-        self.arima_results = {'y_pred'          : self.y_pred,
-                             'y_pred_intervals' : final_res_kt,
-                             'params'           : arima_model.get_fitted_params(),
-                             'pred_mort_rates'  : np.exp(self.mort_data)}       
-        return self.arima_results
-
-
-        
+d_rate = pd.DataFrame(data = n_D.values/n_POP.values,
+                      columns = list(globalenv['exp2_clm']),
+                      index = list(globalenv['exp2_row']))
+dat_d_rate = d_rate.copy().loc[[str(i) for i in range(55,89)]]
 #%%
+#Init & FIT R model
+ro.r('LC1 <- lc(link = "logit")')
+ro.r('LCfit <- fit(LC1, data = EWMaleData, ages.fit = 55:89)')
+ro.r('plot(LCfit)')
+ro.r('LCfit_kt <- LCfit$kt')
+ro.r('colnames(LCfit$kt)')
+kt_R = np.array(globalenv['LCfit_kt'])
+
+#%%
+#Init Python model
+lc_py = LC_model()
+
+lc_py.load_data(data = dat_d_rate,data_type='rate')
+
+kt_py = lc_py.k_t
+
+#%%
+svd_res = lc_py.res_SVD_fin
+ax = lc_py.a_x
+bx = lc_py.b_x
+s_1 = lc_py.res_SVD_fin['S'][0]
+'''Lee and Carter also take the first column of U, multiply by D1,1 
+and multiply by the sum of the first row of V
+(to cancel the division) and call that k. 
+This vector captures overall mortality change over time.'''
+
+. mata k = U[,1] * sum(Vt[1,]) * d[1]
+
+mata_k = svd_res['U'][:,0]*np.sum(svd_res['V*'][1,:])*svd_res['S'][0]
+
+mort_data_raw_ = lc_py.mort_data_raw 
+
+ro.r('''LCfit <- fit(lc(), Dxt = EWMaleData$Dxt, Ext = EWMaleData$Ext, 
+             ages = EWMaleData$ages, years = EWMaleData$years, 
+             ages.fit = 55:89)''')
+ro.r('plot(LCfit)')
 
 
+
+
+
+
+ro.r('ages.fit <- 55:89')
+ro.r('wxt <- genWeightMat(ages = ages.fit, years = EWMaleIniData$years,clip = 3)')
+# aaa =  r_to_pd(globalenv['EWMaleIniData'])
+ro.r('LC <- lc(link = "logit")')
+ro.r('LCfit <- fit(LC, data = EWMaleIniData, ages.fit = ages.fit, wxt = wxt)')
+ro.r('plot(LCfit, nCol = 3)')
+ro.r('exxp_1 <- LCfit$Dxt')
+aaa =  r_to_pd(globalenv['EWMaleIniData'])
+ro.r('h <- 15')
+ro.r('kt.LCfit <- t(LCfit$kt)')
+
+
+#%%
 lc_inst = LC_model()
 lc_inst.load_data(fin_dat, data_type='rate')
 data_raw = lc_inst.mort_data_raw
@@ -182,100 +223,11 @@ xx_kt = lc_inst.k_t
 #extract param b_x
 xx_bx = lc_inst.b_x
    
-zzz_pred = lc_inst.fit_predict(n_futures = 15, pred_int =.9) 
+zzz_pred = lc_inst.fit_predict(n_futures = 15, pred_int =.7) 
+lc_inst.plot_kt()
 
-#%% 
-plt_dat = zzz_pred['y_pred_intervals']
-aaaP = sns.lineplot(x      = plt_dat.index.to_list(),
-                    y      = 'kt_log',
-                    data   = plt_dat)
-
-aaaP.fill_between(x =     plt_dat.index.to_list(), 
-                  y1 =    plt_dat['kt_log']+plt_dat['higher_0.9'].to_numpy(), 
-                  y2 =    plt_dat['kt_log']-plt_dat['lower_0.9'].to_numpy(),
-                  alpha = .25)
-
-
-
-
-
-#%% 
-######################################################################
-################# USE ARIMA TO PREDICTI
-######################################################################
-####predict
-
-y = load_airline()
- 
- 
 
 #%%
-########################################################################
-########### MODELING IMPORT
-import pickle
-import pandas as pd
-import os
-r_home = r'C:\Program Files\R\R-4.0.2'
-os.environ['R_HOME'] = r_home
-import rpy2
-rpy2_info = (rpy2.__path__,rpy2.__version__,'R_HOME',r_home)
-print(rpy2_info)
-
-def r_to_pd(smth_r):
-    from rpy2.robjects import pandas2ri
-    from rpy2.robjects.conversion import localconverter
-    import rpy2.robjects as ro
-    
-    with localconverter(ro.default_converter + pandas2ri.converter):
-        smth_pd   = ro.conversion.rpy2py(smth_r)
-    return smth_pd
-
-def pd_to_r(smth_pd):
-    from rpy2.robjects import pandas2ri
-    from rpy2.robjects.conversion import localconverter
-    import rpy2.robjects as ro
-    
-    with localconverter(ro.default_converter + pandas2ri.converter):
-        smth_r   = ro.conversion.py2rpy(smth_pd)
-    return smth_r
-
-from rpy2.robjects import globalenv
-from rpy2.robjects.packages import importr
-from rpy2.robjects import pandas2ri
-from rpy2.robjects.conversion import localconverter
-import rpy2.robjects as ro
-
-base = importr('base')
-utils = importr('utils')
-brms = importr('brms')
-sjPlot = importr('sjPlot')
-ro.r('if (require(StMoMo) == FALSE) {install.packages("StMoMo")}')
-ro.r('if (require(demography) == FALSE) {install.packages("demography")}')
-ro.r('if (require(HMDHFDplus) == FALSE) {install.packages("HMDHFDplus")}')
-ro.r('if (require(MTS) == FALSE) {install.packages("MTS")}')
-ro.r('if (require(stats) == FALSE) {install.packages("stats")}')
-
-importr('MTS')
-importr('demography')
-importr('StMoMo')
-importr('HMDHFDplus')
-importr('stats')
-#%%
-##############################################################################
-############################ Fit Standard data from STMOMO
-ro.r('LC <- lc(link = "logit")')
-ro.r('EWMaleIniData <- central2initial(EWMaleData)')
-
-ro.r('EWMaleData$Dxt')
-ro.r('EWMaleData$Ext')
-
-ro.r('ages.fit <- 55:89')
-ro.r('wxt <- genWeightMat(ages = ages.fit, years = EWMaleIniData$years,clip = 3)')
-ro.r('LCfit <- fit(LC, data = EWMaleIniData, ages.fit = ages.fit, wxt = wxt)')
-ro.r('plot(LCfit, nCol = 3)')
-
-ro.r('h <- 15')
-ro.r('kt.LCfit <- t(LCfit$kt)')
 
 lc_res_stmomo = np.array(globalenv['kt.LCfit'])
 
@@ -337,9 +289,7 @@ ro.r('pred.ktdiff.LCfit')
 
 
 #%%
-# ro.r('''wDE_DAT <- readHMDweb(CNTRY = "DEUTFRG", 
-#                               username = "hdgniehaus@arcor.de",
-#                               password = "Moi_quiee148")''')
+
 
 ro.r('''wDE_DAT <- readHMD("Mx_1x1.txt")''')
 # ro.r('''aaa <- demogdata(wDE_DAT)''')
@@ -391,31 +341,4 @@ bbb = np.array(globalenv['export2'])
 #                                    raw = True)
 
 
-# n_future_years = 15
-# forecaster = ARIMA(order=(1, 0, 1), 
-#                    seasonal_order=(0, 0, 0, 0))
-# forecaster.fit(k_t)  
-# future_H = [i for i in range(n_future_years)]
-# y_pred = forecaster.predict(fh=future_H) 
-# y_pred_L = list(y_pred)
-
-# fin_data_pred = fin_dat.copy()
-# last_dat = int(fin_dat.columns.max())
-
-# for fut_year in range(n_future_years):
-#     term1 = a_time+res_SVD_scp_fin['S'][0]
-#     term2 = b_x*y_pred_L[fut_year]
-#     term3 = np.exp(term1*term2)
-#     fin_data_pred[str(last_dat+(fut_year+1)+1)] = term3
-    
-# zzzz = forecaster.get_fitted_params() 
-# xxxx = forecaster.predict_interval(fh = future_H)   
-
-# #extract param k_t
-# k_t = pd.DataFrame(res_SVD_scp_fin['U'])[0].values
-# xx_kt = lc_inst.k_t
-# #extract param b_x
-# b_x = pd.DataFrame(res_SVD_scp_fin['V*']).T[0].values
-# xx_bx = lc_inst.b_x
- 
   
---
+++ mort_model_class.py
@@ -0,0 +1,113 @@
+
+
+class LC_model:
+    
+    def __init__(self):
+        print('model init')
+ 
+        self.fit = False
+    
+    def load_data(self,data,data_type):
+        import pandas as pd
+        import numpy as np
+        from scipy.linalg import svd
+        error_msg_datatype = 'input/data-type not supported.'
+        error_mdg_data = '''Data needs to be dataframe of mortality rates.
+                            Years as COLUMNS and Age as ROWS'''
+        if data_type == 'rate':
+            self.mort_data_raw = data.dropna(axis = 0,
+                                             how = 'any').apply(func = np.log,
+                                                                axis = 1,
+                                                                raw = True)
+                                                  
+            self.a_x = self.mort_data_raw.mean(axis = 'columns')
+            
+            a_xt_raw = pd.DataFrame(index = [i for i in self.mort_data_raw.index])
+            for year in self.mort_data_raw:
+                curr_clm = self.mort_data_raw[year].values - self.a_x.values
+                a_xt_raw[year] = curr_clm.tolist()
+            
+            
+            self.a_xt = a_xt_raw.copy().T
+            
+            res_SVD = svd(self.a_xt.values,
+                          full_matrices=True)     
+            
+            ### Compute SVD of A_xt
+            # USV* = SVD(A_xt)  
+            # U == left-singular vectors
+            # S == singular values 
+            # V* == right-singular vectors                                               
+            self.res_SVD_fin = {'U' : res_SVD[0],
+                                'S' : res_SVD[1],
+                                'V*' : res_SVD[2]}
+            
+            #extract param k_t
+            self.k_t = pd.DataFrame(self.res_SVD_fin['U'])[0].values
+            self.k_pre = pd.DataFrame(self.res_SVD_fin['U'])[0].values 
+            #extract param b_x
+            self.b_x = pd.DataFrame(self.res_SVD_fin['V*']).T[0].values        
+        else:
+            print()
+        
+    def fit_predict(self,n_futures, pred_int):
+        import pandas as pd
+        import numpy as np
+        from scipy.linalg import svd
+        from sktime.forecasting.arima import ARIMA
+        self.future_N = n_futures
+        self.pred_interval = pred_int
+        arima_model = ARIMA(order=(1, 0, 1)).fit(self.k_t)
+        
+        self.arima_model = arima_model
+        self.future_lags = [i for i in range(self.future_N)]
+        self.y_pred = arima_model.predict(fh=self.future_lags).flatten().tolist()
+        self.y_pred_int = arima_model.predict_interval(fh = self.future_lags,
+                                                          coverage = pred_int)
+
+        
+        self.mort_data = self.mort_data_raw.copy()
+        last_dat = int(self.mort_data.columns.max())
+        
+        for fut_year in range(self.future_N):
+            term1 = self.a_x+self.res_SVD_fin['S'][0]
+            term2 = self.b_x*self.y_pred[fut_year]
+            term3 = term1*term2
+            self.mort_data[str(last_dat+(fut_year+1))] = term3
+       
+        self.all_Y = self.mort_data.columns.tolist()
+        final_res_kt = pd.DataFrame(index = self.all_Y)
+        final_res_kt['kt_log'] = self.k_t.flatten().tolist() + self.y_pred
+        print(final_res_kt)
+        for i,j in zip(self.y_pred_int,[f'lower_{pred_int}',f'higher_{pred_int}']):
+            dat = self.y_pred_int[i].tolist() + self.y_pred
+            final_res_kt[j] = [0 for i in range(len(self.k_t.flatten().tolist()) )]+self.y_pred_int[i].tolist()
+       
+
+        self.arima_results = {'y_pred'          : self.y_pred,
+                             'y_pred_intervals' : final_res_kt,
+                             'params'           : arima_model.get_fitted_params(),
+                             'pred_mort_rates'  : np.exp(self.mort_data)}       
+        self.fit = True
+        return self.arima_results
+
+    def plot_kt(self):
+        import pandas as pd
+        import numpy as np
+        from scipy.linalg import svd
+        import seaborn as sns
+        if self.fit == False:
+            print('ERROR ARIMA NOT FIT')
+        elif self.fit == True:
+            plt_ = self.arima_results['y_pred_intervals']
+            plt_sns = sns.lineplot(x      = plt_.index.to_list(),
+                                   y      = 'kt_log',
+                                   data   = plt_)
+            
+            plt_sns.fill_between(x =     plt_.index.to_list(), 
+                                 y1 =    plt_['kt_log']+plt_[f'higher_{self.pred_interval}'].to_numpy(), 
+                                 y2 =    plt_['kt_log']-plt_[f'lower_{self.pred_interval}'].to_numpy(),
+                                 alpha = .25)
+            plt_sns.set_xticklabels(plt_sns.get_xticklabels(),rotation=30) 
+  
+
